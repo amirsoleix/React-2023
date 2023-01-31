89005355e04b41c9c958d93606d709a7d1ea8821
Amir Soleimanifar <asoleix@gmail.com>
2023-01-31 22:38:57 +0330

    Add Registration popup and modify footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,25 +4,25 @@ import classNames from 'classnames';
 
 import { getModifiers } from 'src/utils/classname';
 import Icon from 'src/components/Icon';
-import { VALID_SIZES } from 'src/components/Icon/constants';
+// import { VALID_SIZES } from 'src/components/Icon/constants';
 
 import { SOCIAL_LINKS } from './constants';
 
 import './Footer.scss';
 
 function Footer({ hasBottomNav }) {
-  function renderLogo() {
-    return (
-      <a href='/' className='footer__link footer__logo'>
-        <Icon name='social-sonnat-thin' size={VALID_SIZES.XL} />
-      </a>
-    );
-  }
+  // function renderLogo() {
+  //   return (
+  //     <a href='/' className='footer__link footer__logo'>
+  //       <Icon name='social-sonnat-thin' size={VALID_SIZES.XL} />
+  //     </a>
+  //   );
+  // }
 
   function renderSocial(item) {
     return (
       <a href={item.link} className='footer__social' key={item.iconName}>
-        <Icon name={item.iconName} />
+        <Icon name={item.iconName} size='XL' />
       </a>
     );
   }
@@ -34,18 +34,17 @@ function Footer({ hasBottomNav }) {
   return (
     <footer className={getClassname()}>
       <div className='footer__right'>
-        {renderLogo()}
         <div className='footer__links'>
           <a href='https://sharif.edu' className='footer__link'>
-            درباره ما
+            About Us
           </a>
           <hr className='footer__divider' />
           <a href='https://ee.sharif.edu' className='footer__link'>
-            سایت رسمی دانشکده مهندسی برق
+            Electrical Engineering Department Official Website
           </a>
           <hr className='footer__divider' />
           <a href='https://ee.sharif.edu/~resana' className='footer__link'>
-            صفحه اصلی انجمن عملی و فرهنگی رسانا
+            Resana Association Website
           </a>
         </div>
       </div>
